83cee88635b5a37c1daa645505856c72c0233d5a
qiming <745685567@qq.com>
2016-08-29 09:22:54 +0800

    创建项目

--- MyNews/ViewController.swift
+++ MyNews/ViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        query()
     }
 
     func query() {
+        let url = "https://v.juhe.cn/toutiao/index"
+        Alamofire.request(.GET, url, parameters: ["key": "97564c8be33d813c207d878e8534699b", "type": "yule"], encoding: .URL, headers: nil).responseJSON {
+            let v = $0.result.value
+            let json = JSON(v!)
+                print(json)
+        }
         
     }
     
